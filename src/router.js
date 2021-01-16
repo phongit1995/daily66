ee65544c316030f672c1e2ad325c34109e872201
phong  nguyen <phongit1995@gmail.com>
2021-01-16 10:47:05 +0700

    add receive data

--- src/router.js
+++ src/router.js
@@ -40,8 +40,8 @@ router.post("/tranfer-account",async(req,res)=>{
 })
 router.post("/find-history-agent-send",async(req,res)=>{
     try {
-        const {username,fromdate,todate}=req.body;
-        if(username==undefined ||fromdate==undefined || todate==undefined  ){
+        const {username}=req.body;
+        if(username==undefined  ){
             throw "Lỗi Thiếu Data" ;
         }
         const userSend = await getUserData(username);
@@ -51,9 +51,7 @@ router.post("/find-history-agent-send",async(req,res)=>{
         const timeNow = moment().format("MM/DD/YYYY")
         const cookieCloudflare = await getCookieCloudflare();
         console.log(cookieCloudflare);
-        const resultHistory = await findHistoryAgent(cookieCloudflare.cookie,cookieCloudflare.userArgent,userSend.cookie,timeNow,timeNow);
-        //const resultHistory = await findHistoryAgentSend('__cfduid=d17850110bd2b097fb29753f8a17859bf1610703359; cf_clearance=ef15af2ef9c38fd1bad56480b4fc04c20459b68c-1610703359-0-150','Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36',userSend.cookie,fromdate,todate);
-        
+        const resultHistory = await findHistoryAgentSend(cookieCloudflare.cookie,cookieCloudflare.userArgent,userSend.cookie,timeNow,timeNow);
         res.status(200).json({data:resultHistory});
     } catch (error) {
         res.status(400).json({error:error.toString()});
@@ -61,18 +59,18 @@ router.post("/find-history-agent-send",async(req,res)=>{
 })
 router.post("/find-history-agent-receive",async(req,res)=>{
     try {
-        const {username,fromdate,todate}=req.body;
-        if(username==undefined ||fromdate==undefined || todate==undefined  ){
+        const {username}=req.body;
+        if(username==undefined ){
             throw "Lỗi Thiếu Data" ;
         }
         const userSend = await getUserData(username);
         if(!userSend){
             throw "user send do not have data";
         }
-        // const cookieCloudflare = await getCookieCloudflare();
-        // console.log(cookieCloudflare);
-        //const resultHistory = await findHistoryAgent(cookieCloudflare.cookie,cookieCloudflare.userArgent,userSend.cookie,fromdate,todate);
-        const resultHistory = await findHistoryAgentReceive('__cfduid=d17850110bd2b097fb29753f8a17859bf1610703359; cf_clearance=ef15af2ef9c38fd1bad56480b4fc04c20459b68c-1610703359-0-150','Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36',userSend.cookie,fromdate,todate);
+        const cookieCloudflare = await getCookieCloudflare();
+        const timeNow = moment().format("MM/DD/YYYY")
+        const resultHistory = await findHistoryAgentReceive(cookieCloudflare.cookie,cookieCloudflare.userArgent,userSend.cookie,timeNow,timeNow);
+        //const resultHistory = await findHistoryAgentReceive('__cfduid=d17850110bd2b097fb29753f8a17859bf1610703359; cf_clearance=ef15af2ef9c38fd1bad56480b4fc04c20459b68c-1610703359-0-150','Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36',userSend.cookie,fromdate,todate);
         
         res.status(200).json({data:resultHistory});
     } catch (error) {
