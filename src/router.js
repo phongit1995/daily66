7cbb4722be5c3fc30c7fa0d55fbe826626ec73ae
phongit <phongit1995@gmail.com>
2021-01-20 21:51:08 +0700

    fix data

--- src/router.js
+++ src/router.js
@@ -34,7 +34,7 @@ router.post("/tranfer-account",async(req,res)=>{
         throw "user send do not have data";
     }
     const cookieCloudflare = await getCookieCloudflare(proxy);
-    const resultTranfer =  await transferAccount(cookieCloudflare,USER_ARGENT,userSend.cookie,usernamereceive,amount,reason)
+    const resultTranfer =  await transferAccount(cookieCloudflare,USER_ARGENT,userSend.cookie,usernamereceive,amount,reason,proxy)
     res.status(200).json({data:resultTranfer});
     } catch (error) {
         res.status(400).json({error:error.toString()});
@@ -71,7 +71,7 @@ router.post("/find-history-agent-receive",async(req,res)=>{
         }
         const cookieCloudflare = await getCookieCloudflare(proxy);
         const timeNow = moment().format("MM/DD/YYYY")
-        const resultHistory = await findHistoryAgentReceive(cookieCloudflare,cookieCloudflare,USER_ARGENT,userSend.cookie,timeNow,timeNow,proxy);
+        const resultHistory = await findHistoryAgentReceive(cookieCloudflare,USER_ARGENT,userSend.cookie,timeNow,timeNow,proxy);
         //const resultHistory = await findHistoryAgentReceive('__cfduid=d17850110bd2b097fb29753f8a17859bf1610703359; cf_clearance=ef15af2ef9c38fd1bad56480b4fc04c20459b68c-1610703359-0-150','Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36',userSend.cookie,fromdate,todate);
         
         res.status(200).json({data:resultHistory});
